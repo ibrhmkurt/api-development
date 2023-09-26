655b80013b102782826978b2852625a1e5154e88
ibrhmkurt <ibrahimkurt@protonmail.com>
2023-09-26 12:58:11 +0300

    items listing

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $perPage = $request->get('perPage', 5);
+        $items = Item::paginate($perPage);
+
+        return response()->json($items);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('index', []);
+Route::get('items', [\App\Http\Controllers\Api\ItemController::class, 'index']);
